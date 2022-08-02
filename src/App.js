12a7dc5493d87e5be796ba969c7d2c5cb1bdf0ad
mutemip <paulmutemi2030@gmail.com>
2022-08-03 00:39:46 +0300

    added loading state variable

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import axios from 'axios'
 
 function App() {
   const [storeItem, setStoreItem] = useState([])
+  const [loading, setLoading] = useState(true)
   
     useEffect(() => {
       //get method
       axios.get("https://fakestoreapi.com/products")
-      .then(({data}) => setStoreItem(data))
+      .then(({data}) => {
+        setLoading(false)
+        setStoreItem(data)
+      })
     }, [])
   return (
     <div>
-        <Store items={storeItem} onItemAdd={(itemData) => {setStoreItem([...storeItem, itemData])}}/>
+        <Store items={storeItem} 
+        onItemAdd={(itemData) => {setStoreItem([...storeItem, itemData])}}
+        loading={loading}/>
     </div>
   );
 }
